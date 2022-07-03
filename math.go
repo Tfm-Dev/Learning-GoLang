e114c47310e88a498a1f9a55734f07a59bd9c522
Mourathiago <moura.francisco.thiago@hotmail.com>
2022-07-03 14:23:37 -0400

    Basic math operations

---
+++ math.go
@@ -0,0 +1,24 @@
+package main
+
+import "fmt"
+
+func main() {
+	//Basic values
+	numOne := 10
+	numbTwo := 2
+
+	//Basic math operations
+	add := numOne + numbTwo
+	sub := numOne - numbTwo
+	mult := numOne * numbTwo
+	div := numOne / numbTwo
+	mod := numOne % numbTwo
+
+	//Output
+	fmt.Println("addition:", add)
+	fmt.Println("Subtraction:", sub)
+	fmt.Println("Multiplication:", mult)
+	fmt.Println("Division:", div)
+	fmt.Println("Module:", mod)
+
+}
